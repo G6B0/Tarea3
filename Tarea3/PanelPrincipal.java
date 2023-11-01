d406e16cc91da88c7ac1fe4d82e398771b647718
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-11-01 11:53:07 -0300

    Se agregan listeners para poder manejar las compras.

--- Tarea3/PanelPrincipal.java
+++ Tarea3/PanelPrincipal.java
@@ -1,17 +1,29 @@
 package Tarea3;
 
-import Tarea2.Expendedor;
+
+import Tarea2.NoHayProductoException;
+import Tarea2.PagoIncorrectoException;
+import Tarea2.PagoInsuficienteException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements MouseListener {
     private PanelComprador com;
     private PanelExpendedor exp;
 
-    public PanelPrincipal() {
+    public PanelPrincipal() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         exp = new PanelExpendedor(3);
-        com = new PanelComprador(300, 550);
+        com = new PanelComprador(600, 600,3);
+        this.addMouseListener(this);
+        com.getComprar1().addMouseListener(this);
+        com.getComprar2().addMouseListener(this);
+        com.getComprar3().addMouseListener(this);
+        com.getComprar4().addMouseListener(this);
+        com.getComprar5().addMouseListener(this);
+
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -23,19 +35,67 @@ public class PanelPrincipal extends JPanel {
         }
         if (com != null) {
             add(com);
-            com.setLocation(700,0);
+            com.setLocation(900,0);
             com.paintComponent(g);
         }
     }
     
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Mi Aplicación");
-            PanelPrincipal panelPrincipal = new PanelPrincipal();
+            PanelPrincipal panelPrincipal = null;
+            try {
+                panelPrincipal = new PanelPrincipal();
+            } catch (NoHayProductoException ex) {
+                JOptionPane.showMessageDialog(null,"Nos quedamos sin producto de este tipo :c, compra no realizada");
+            } catch (PagoIncorrectoException ex) {
+                JOptionPane.showMessageDialog(null,"Pago Incorrecto, compra no realizada");
+            } catch (PagoInsuficienteException ex) {
+                JOptionPane.showMessageDialog(null,"Pago Insuficiente, compra no realizada");
+            }
             frame.add(panelPrincipal);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1900, 1080);
             frame.setVisible(true);
         });
-    }*/
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getComponent()==com.getComprar1 () && com.getRespuesta()== JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Estamos trabajando para ti :) danos unos segundos");
+        }
+        if(e.getComponent()==com.getComprar2 () && com.getRespuesta()== JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Estamos trabajando para ti :) danos unos segundos");
+        }
+        if(e.getComponent()==com.getComprar3 () && com.getRespuesta()== JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Estamos trabajando para ti :) danos unos segundos");
+        }
+        if(e.getComponent()==com.getComprar4 () && com.getRespuesta()== JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Estamos trabajando para ti :) danos unos segundos");
+        }
+        if(e.getComponent()==com.getComprar5 () && com.getRespuesta()== JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"Estamos trabajando para ti :) danos unos segundos");
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
